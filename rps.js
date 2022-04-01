3bd91739709fb071fa18accb9b9b2734a17e28ce
Londonexmachina <london.ex.machina@gmail.com>
2022-03-31 19:57:55 -0700

    Create playRound function

--- rps.js
+++ rps.js
@@ -11,3 +11,16 @@ function computerPlay() {
             return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    let playerUpperCase = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (playerUpperCase === computerSelection) {
+        return "Draw!"
+    } else if (playerUpperCase == "Rock" && computerSelection == "Scissors" ||
+               playerUpperCase == "Paper" && computerSelection == "Rock" || 
+               playerUpperCase == "Scissors" && computerSelection == "Paper") {
+        return "You Win! " + playerUpperCase + " beats " + computerSelection + "!"
+    } else {
+        return "You Lose! " + computerSelection + " beats " + playerUpperCase + "!"
+    }
+}
